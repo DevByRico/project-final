52e59e9564509934251847c481dfdd4df0ac1d80
RicardoNicolau <ricardonicolau2001@gmail.com>
2025-10-18 22:03:19 +0200

    Fix: remove double BrowserRouter to fix navigation

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,16 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-// dina providers
-import { AuthProvider } from "./store/auth";
-import { BookingProvider } from "./store/booking";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <BookingProvider>
-        <App />
-      </BookingProvider>
-    </AuthProvider>
+    <App />
   </React.StrictMode>
 );
