593371b93d7d8667da7e11b427a7594687f3fdbe
RicardoNicolau <ricardonicolau2001@gmail.com>
2025-10-18 22:06:06 +0200

    Fix: restore providers but keep single BrowserRouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { AuthProvider } from "./store/auth";
+import { BookingProvider } from "./store/booking";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <BookingProvider>
+        <App />
+      </BookingProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
